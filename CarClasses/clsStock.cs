dec9c765524120d1f2b86db21e561a327fe51a9e
P2524416 <P2524416@my365.dmu.ac.uk>
2021-02-18 17:32:46 +0000

    Week 19 Done

--- CarClasses/clsStock.cs
+++ CarClasses/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace CarClasses
+﻿using System;
+
+namespace CarClasses
 {
     public class clsStock
     {
+        public bool Availability { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int ModelNo { get; set; }
+        public string BHP { get; set; }
+        public string CarModel { get; set; }
+        public string Price { get; set; }
     }
 }
---
+++ CarFrontOffice/AStock.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CarClasses;
+
+public partial class AStock : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsAstock
+        clsStock AStock = new clsStock();
+        //capture the customer stock no
+        AStock.CarModel = txtCarModel.Text;
+        AStock.BHP = txtBHP.Text;
+        AStock.Price = txtPrice.Text;
+        AStock.DateAdded = DateTime.Now;
+        //store the bjp in th esession
+        Session["AStock"] = AStock;
+        //redirect the viewer page
+        Response.Redirect("StockViewer.aspx");
+
+
+    }
+}
